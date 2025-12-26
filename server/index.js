b1c435f137b061ef1c8cd4e60ac4855c89267c89
Sara Fraihat <sara.fraihat5@gmail.com>
2025-12-26 17:05:28 +0300

    Person 1: Backend Security, MongoDB Atlas connection, and Session setup complete

---
+++ server/index.js
@@ -0,0 +1,41 @@
+// index.js
+const authRoutes = require('./routes/auth');
+const express = require('express');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo').default; 
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+
+//const mongoUrl = "mongodb+srv://test:test@cluster0.mongodb.net/myDB?retryWrites=true&w=majority";
+const mongoUrl = process.env.MONGO_URI;
+app.use(session({
+    secret: 'sara_secret',
+    resave: false,
+    saveUninitialized: false,
+
+store: MongoStore.create({
+    mongoUrl: process.env.MONGO_URI,
+    collectionName: 'sessions'
+}),
+   cookie: { 
+    maxAge: 1000 * 60 * 60 * 24, 
+    httpOnly: true,
+    secure: false 
+}
+}));
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch(err => console.log("Connection Error: ", err));
+
+ app.use('/api/auth', authRoutes);
+
+app.get('/hello', (req, res) => {
+    res.send("Hello from my server ✅");
+});
+
+app.listen(5000, () => console.log(' Server running on port 5000'));
---
+++ server/routes/auth.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+
+// register
+router.post('/register', async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({ username, email, password: hashedPassword });
+        await newUser.save();
+        res.status(201).json({ message: "User registered successfully!" });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+module.exports = router;
+//login code
+router.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) return res.status(404).json({ message: "User not found!" });
+
+        // compare
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(400).json({ message: "Invalid credentials!" });
+
+        // creat the session 
+        req.session.userId = user._id;
+        res.json({ message: "Logged in successfully!", user: { username: user.username } });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
